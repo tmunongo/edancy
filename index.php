e3f162beee589820f1cd2f0f268328c7c57cf1d7
Tawanda Munongo <ejmunongo@gmail.com>
2023-06-12 17:07:57 +0200

    update nginx config

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home - Edancy Consultancy</title>
     <!-- /* Import the CSS file into your HTML */ -->
-    <link rel="stylesheet" href="styles/main.css">
+    <link rel="stylesheet" type="text/css" href="styles/main.css">
     <!-- Bootstrap v5 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <!-- JQuery CDN -->
